23bc11b07487331429f4f33963b10caaa5bc69f6
draxya <68575901+draxya@users.noreply.github.com>
2022-02-13 19:05:56 +0300

    Update index.tsx

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,14 +6,14 @@ export const Navbar: FC = () => {
       <div className="flex md:w-7/12 w-10/12 justify-between">
         <div className="flex items-center">
           <p className="text-2xl font-bold text-gray-200">
-            rexulec.
+            draxya.
           </p>
         </div>
         <div className="flex items-center text-gray-300">
           <a href="" className="mr-3">
             Projects
           </a>
-          <a href="mailto:hi@rexulec.com" className="px-5 p-2 rounded-full bg-[#1f1f1f]">
+          <a href="mailto:contactdraxya@gmail.com" className="px-5 p-2 rounded-full bg-[#1f1f1f]">
             Contact
           </a>
         </div>
