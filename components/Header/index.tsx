5061bde138f1f5d7ae5a100ee32bd70ab9ee48c0
draxya <68575901+draxya@users.noreply.github.com>
2022-02-21 20:18:11 +0300

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -15,17 +15,17 @@ export const Header:FC<IProps> = function({title}){
         <meta name="description" content="I'm a full stack developer with over 2 years of experience, 17 y/o, from Ankara."/>
 
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.draxya.cf"/>
+        <meta property="og:url" content="https://www.draxya.com"/>
         <meta property="og:title" content="draxya."/>
         <meta property="og:description" content="I'm a full stack developer with over 2 years of experience, 17 y/o, from Ankara."/>
-        <meta property="og:image" content="https://www.draxya.cf/icon.png"/>
+        <meta property="og:image" content="https://www.draxya.com/icon.png"/>
         <meta property="theme-color" content="#9932CC"/>
 
-        <meta property="twitter:url" content="https://www.draxya.cf"/>
-        <meta property="twitter:title" content="www.draxya.cf"/>
+        <meta property="twitter:url" content="https://www.draxya.com"/>
+        <meta property="twitter:title" content="www.draxya.com"/>
         <meta property="twitter:description" content="I'm a full stack developer with over 2 years of experience, 17 y/o, from Ankara."/>
-        <meta property="twitter:image" content="https://www.draxya.cf/icon.png"/>
-        <meta property="twitter:domain" content="www.draxya.cf"/>
+        <meta property="twitter:image" content="https://www.draxya.com/icon.png"/>
+        <meta property="twitter:domain" content="www.draxya.com"/>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
