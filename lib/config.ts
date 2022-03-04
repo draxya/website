cc9cccd074bfb6ade9c85a1408d7b054d0810ed0
draxya <68575901+draxya@users.noreply.github.com>
2022-03-05 01:29:20 +0300

    Projects Update
    This update brings out new page "/projects".

--- lib/config.ts
+++ lib/config.ts
@@ -1,34 +1,58 @@
 export const CONFIG = {
-    PROJECTS: [
-      {
-        title: "Reponse App",
-        description: "A new way to chat with your communities and friends.",
-        role: "Founder & Creator",
-        what: "I founded Reponse back in 2021 to make comminication more easy than ever!",
-        img: "/projects/reponse.png"
-      },
-      {
-        title: "CodAre",
-        description: "A Discord server for coding community.",
-        role: "Support Team & Staff",
-        what: "I joined the CodAre's support team in 2020 and it was best decision in my life.",
-        img: "/projects/codare.jpg"
-      },
-      {
-        title: "RepeatPay",
-        description: "Innovative payment solution for your websites.",
-        role: "Founder & Creator",
-        what: "I founded RepeatPay back in 2021 to make open-source donating app for your donate pages and etc..",
-        img: "/projects/repeatpay.png"
-      }
-    ] as PROJECTS[],
-  
-  };
-  
-  interface PROJECTS {
-    title: string;
-    description: string;
-    role: string;
-    what: string;
-    img: string;
-  }
+
+  MAINPROJECTS: [
+    {
+      title: "Marill ROV",
+      description: "Unmanned Underwater Vehicle",
+      role: "Team Member & Software Developer",
+      what: "Marill is an UUV made for Teknofest 2022 to reach deep in sea without human activity.",
+      img: "/projects/marill.jpeg",
+      linkto: "https://github.com/Marill-ROV"
+    },
+    {
+      title: "Jutul",
+      description: "A Discord bot for moderation and fun",
+      role: "Founder & Developer",
+      what: "I started making of Jutul back in 2020 and still developinng it.",
+      img: "/projects/jutul.webp",
+      linkto: "https://top.gg/tr/bot/759823414042034178"
+    },
+    {
+      title: "Jutul Music",
+      description: "Jutul's following project with music commands",
+      role: "Founder & Developer",
+      what: "I started making of Jutul Music at 2021 and still working to improve the features of it.",
+      img: "/projects/jutulmusic.webp",
+      linkto: "https://jutulmusicbot.tk"
+    }
+  ] as MAINPROJECTS[],
+
+  SIDEPROJECTS: [
+    {
+      title: "Aesir Minecraft",
+      description: "A brand new survival experience for everyone.",
+      img: "/projects/aesirmc.png",
+      linkto: "aesir",
+      view: "https://discord.gg/VtgeKGh78H",
+      mode: "1"
+    }
+  ] as SIDEPROJECTS[],
+};
+
+interface MAINPROJECTS {
+  title: string;
+  description: string;
+  role: string;
+  what: string;
+  img: string;
+  linkto: string;
+}
+
+interface SIDEPROJECTS {
+  title: string;
+  description: string;
+  img: string;
+  linkto: any;
+  view: string;
+  mode: string;
+}
